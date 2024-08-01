815e02bc70af1cd5b12386c56d144aa143e7d8d8
Harshit Khurana <khurana.harshit2003@gmail.com>
2024-08-02 03:14:54 +0530

    Updated json-display

--- src/app/json-display/page.js
+++ src/app/json-display/page.js
@@ -1,12 +1,21 @@
-"use client"; 
-import { useSearchParams } from 'next/navigation'
- 
-export default function SearchBar() {
-  const searchParams = useSearchParams()
+"use client";
+import React, { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+function SearchBarContent() {
+  const searchParams = useSearchParams();
   const data = searchParams.get('jsonData');
   return (
     <section className="w-full bg-black p-[0] h-full text-white pt-[6%]">
       <pre>{data}</pre>
     </section>
   );
-};
+}
+
+export default function SearchBar() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchBarContent />
+    </Suspense>
+  );
+}
