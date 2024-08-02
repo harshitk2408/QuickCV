8d6858bf69b183a92cfeae7267e82b76cb628cac
Harshit Khurana <122016742+harshitk2408@users.noreply.github.com>
2024-08-02 23:25:57 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "QuickCV",
-  description: "Generated CV in seconds",
+  description: "Generate CV in seconds",
 };
 
 export default function RootLayout({ children }) {
